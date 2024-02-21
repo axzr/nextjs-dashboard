d62206933bcbf4c4c217ea860186a477e09c76d5
axzr <ad@mrbrooks.org>
2024-02-21 09:31:03 +0000

    [broken] wip authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { AuthError } from 'next-auth';
+import { signIn } from '@/auth';
 
 // This is temporary until @types/react-dom is updated
 export type State = {
@@ -101,5 +103,52 @@ export async function deleteInvoice(id: string) {
     return { message: 'Deleted Invoice.' };
   } catch (error) {
     return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
+}
+
+// export async function authenticate(
+//   prevState: string | undefined,
+//   formData: FormData,
+// ) {
+//   try {
+//     console.log('calling signIn @@@@');
+
+//     await signIn('credentials', formData);
+
+//     console.log('signIn completed @@@@');
+//   } catch (error) {
+//     console.log('signIn error @@@@', error);
+
+//     if (error instanceof AuthError) {
+//       switch (error.type) {
+//         case 'CredentialsSignin':
+//           return 'Invalid credentials.';
+//         default:
+//           return 'Something went wrong.';
+//       }
+//     }
+//     throw error;
+//   }
+// }
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  let responseRedirectUrl = null;
+  try {
+    console.log('formData', formData);
+    responseRedirectUrl = await signIn('credentials', {
+      ...Object.fromEntries(formData),
+      redirect: false,
+    });
+  } catch (error) {
+    console.log('error', error);
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialSignin';
+    }
+    throw error;
+  } finally {
+    if (responseRedirectUrl) redirect(responseRedirectUrl);
   }
 }
